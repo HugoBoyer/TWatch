cf7ad0c602aeb91d1f30c3062d175f1793b150b3
Hugo <boyer.hugo974@gmail.com>
2025-12-02 21:31:51 +0100

    feat(Home) limitation du contenue du caroussel a 5

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,11 @@ export default function Home () {
     const { data: trendingShows, loading, error } = useFetchData("http://localhost:3000/api/trending");
     if (loading) return <div>Chargement des tendances...</div>;
     if (error) return <div>Erreur: {error}</div>;
+    const limitedTrendingShows = trendingShows.slice(0, 5)
     return (
         <>
             <Header />  
-            <Caroussel items={trendingShows}/>
+            <Caroussel items={limitedTrendingShows}/>
             <ChoiceSerieFilm />
         </>
     )
